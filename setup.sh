74324377c136e32f3db2d10bdedf2a7870e86596
GeneralLPrince <53830063+GeneralLPrince@users.noreply.github.com>
2024-09-27 12:35:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Update package lists and install Python and Git
+# Install Python and Git
 install_python() {
     if ! command -v python3 &> /dev/null; then
         sudo apt-get update
@@ -20,24 +20,21 @@ install_git
 
 # Clone checkuit repo if not already cloned
 TARGET_DIR="/workspaces/checkuit"
-
 if [ ! -d "$TARGET_DIR" ]; then
     git clone https://github.com/generallprince/checkuit.git "$TARGET_DIR"
 fi
 
 # Add alias to .bash_aliases if not already present
-alias checkuit='python3 /workspaces/checkuit/checkuit.py'
+ALIAS="alias checkuit='python3 $TARGET_DIR/checkuit.py'"
 
 if ! grep -Fxq "$ALIAS" ~/.bash_aliases; then
     echo "$ALIAS" >> ~/.bash_aliases
-    echo "Alias added to .bash_aliases"
 fi
 
 # Source both .bashrc and .bash_aliases
-if [ -f ~/.bashrc ]; then
-    source ~/.bashrc
-fi
-
 if [ -f ~/.bash_aliases ]; then
     source ~/.bash_aliases
 fi
+
+# Automatically reload the shell for the alias to take effect
+exec bash
